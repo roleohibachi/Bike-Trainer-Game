540b4a3dadcc2d11748596c3cf08a97ff8d0dd98
roleohibachi <roleohibachi@gmail.com>
2017-12-21 22:55:24 -1000

    neaten

--- Bike-Trainer-Game.ino
+++ Bike-Trainer-Game.ino
@@ -1,50 +1,81 @@
+//#include <TimerOne.h>
+#include <Joystick.h>
 #include <ArduinoNunchuk.h>
 
-
-int throttleMax = 50;  // starter value, roughly observed. Vary with pot?
+const int samplePeriod = 250;  // msec
 const int hatMax = 1024;  // TODO: ???
-
 const int hallPin = 3;  // must be interruptable. 0, 1, 2, 3, 7 for Arduino
                         // Micro
 
-volatile int lastRev = 0;
+int throttleMax = 30;  // mi/hr. TODO:Vary with pot?
+const int mphPerRpm = 150; //calculated 197.416, we'll see!
+volatile int mph = 0;
 volatile int revs = 0;
 
 volatile int xmax = 0;
 volatile int zmax = 0;
-volatile int hat = -1;
+volatile int hat = JOYSTICK_HATSWITCH_RELEASE;
 
-ArduinoNunchuk nunchuck = ArduinoNunchuk();
+ArduinoNunchuk nunchuck = ArduinoNunchuk(); //weird spelling
+
+Joystick_ Joystick(  // optimized for nunchuck+bike throttle, I think
+    JOYSTICK_DEFAULT_REPORT_ID,  // hidReportId
+    JOYSTICK_TYPE_JOYSTICK,      // joystickType
+    2,  // buttonCount
+    0,  // hatSwitchCount
+    true,   // includeXAxis
+    true,   // includeYAxis
+    false,  // includeZAxis
+    false,  // includeRxAxis
+    false,  // includeRyAxis
+    false,  // includeRzAxis
+    false,  // includeRudder
+    true,   // includeThrottle
+    false,  // includeAccelerator
+    false,  // includeBrake
+    false   // includeSteering
+    );
 
 void setup() {
 
   pinMode(hallPin, INPUT);
+
   
   Serial.begin(115200);
-
-  Serial.println();
-  Serial.println();
 
   Serial.print("throttle\t");
   Serial.print("nunZ\tnunC\t");
   Serial.print("nunX\tnunY");
   Serial.print("accX\taccY\taccZ");
-  Serial.println();
+
+  
+  Joystick.setThrottleRange(0, throttleMax);
+  Joystick.setXAxisRange(-127, 127);  // TODO: no idea
+  Joystick.setYAxisRange(-127, 127);  // TODO: no idea
+  Joystick.begin(false);  // do not auto-update; I'll send updates via
+                          // setThrottle(value) etc
 
   nunchuck.init();
 
-  cli();  // disable interrupts. DO NOT Serial.println() until interrupts are back on, or a watchdog will get you.
-  msecTimer(250);
+  cli();  // disable interrupts
+  //Timer1.initialize(250*1000); //microseconds = 250msec = 0.25sec
+  //Timer1.attachInterrupt(timer1ISR); 
+  //TODO: Timer1 library isn't working on the Uno. Check if it plays nice on Micro.
+  // It chooses prescale 8x, but sets WGM13 instead of WGM12
+  // which looks like it would be a PWM mode instead of CTC.
+  
   attachInterrupt(digitalPinToInterrupt(hallPin), throttleISR, RISING);
+  msecTimer(250);
   sei();  // allow interrupts
 }
 
 void loop() {
   // throttle
   // counted in throttleISR
-  // lastrev updated in timer ISR
-  Serial.print(lastRev);
+  // mph updated in timer ISR
+  Serial.print(mph);
   Serial.print("\t");
+  Joystick.setThrottle(mph);
 
   // nunchuck
   nunchuck.update();
@@ -64,17 +95,25 @@ void loop() {
   Serial.print(nunchuck.accelZ, DEC);
   Serial.println();
 
+  Joystick.setButton(0, nunchuck.zButton);
+  Joystick.setButton(1, nunchuck.cButton);
+  Joystick.setXAxis(nunchuck.analogX);
+  Joystick.setYAxis(nunchuck.analogY);
+
   // accelerometer hat
   // poll for max value
   // update value in timer ISR
   xmax = (abs(nunchuck.accelX) > abs(xmax)) ? nunchuck.accelX : xmax;
   zmax = (abs(nunchuck.accelZ) > abs(zmax)) ? nunchuck.accelZ : zmax;
+  Joystick.setHatSwitch(0, hat);
+
+  Joystick.sendState();
 }
 
 ISR(TIMER1_COMPA_vect) {  // timer1 interrupt
   cli();  // so this interrupt doesn't get interrupted mid-write by the other
           // one
-  lastRev = revs;
+  mph = revs*mphPerRpm;
   revs = 0;
 
   // TODO: test this, it's sloppy
@@ -83,46 +122,47 @@ ISR(TIMER1_COMPA_vect) {  // timer1 interrupt
   int hatz =
       ((zmax - 1) / hatMax) * 4;  // should result in -4, 0, 4 vertical twitch
 
-#define HATLEFT -1
-#define HATUP -4
-#define HATDOWN 4
-#define HATRIGHT 1
-#define NOHAT 0
+  
+  #define HATLEFT -1
+  #define HATUP -4
+  #define HATDOWN 4
+  #define HATRIGHT 1
+  #define NOHAT 0
 
   switch (hatx + hatz) {
-    case NOHAT:
-      hat = -1;
+    case NOHAT  :
+      hat = JOYSTICK_HATSWITCH_RELEASE;
       break;
-    case HATUP:
+    case HATUP  :
       hat = 0;
       break;
-    case HATUP + HATRIGHT:
+    case HATUP + HATRIGHT  :
       hat = 45;
       break;
-    case HATRIGHT:
+    case HATRIGHT  :
       hat = 90;
       break;
-    case HATDOWN + HATRIGHT:
+    case HATDOWN + HATRIGHT  :
       hat = 135;
       break;
-    case HATDOWN:
+    case HATDOWN  :
       hat = 180;
       break;
-    case HATDOWN + HATLEFT:
+    case HATDOWN + HATLEFT  :
       hat = 225;
       break;
-    case HATLEFT:
+    case HATLEFT  :
       hat = 270;
       break;
-    case HATUP + HATLEFT:
+    case HATUP + HATLEFT  :
       hat = 315;
       break;
-    default:
+    default  :
       Serial.print("unexpected hat value. Condition: ");
       Serial.print(hatx + hatz);
-      hat = -1;
+      hat = JOYSTICK_HATSWITCH_RELEASE;
   }
-
+  
   sei();  // play ball
 }
 
